61d555e24c3103baa592015ff2503506d355bc9f
Alyxia <alyxia@riseup.net>
2023-02-25 13:12:56 +0100

    [integration] Swap out gofont for Roboto

--- design/font.go
+++ design/font.go
@@ -4,9 +4,10 @@ import (
 	"github.com/golang/freetype"
 	"github.com/uwu/frenyard"
 	"github.com/uwu/frenyard/integration"
+	"github.com/uwu/frenyard/integration/fonts/robotobold"
+	"github.com/uwu/frenyard/integration/fonts/robotoitalic"
+	"github.com/uwu/frenyard/integration/fonts/robotoregular"
 	"golang.org/x/image/font"
-	"golang.org/x/image/font/gofont/gobold"
-	"golang.org/x/image/font/gofont/goregular"
 )
 
 // PageTitleFont for a page title
@@ -14,6 +15,9 @@ var PageTitleFont font.Face
 
 // GlobalFont for most text
 var GlobalFont font.Face
+
+// GlobalItalicFont is GlobalFont but italic
+var GlobalItalicFont font.Face
 
 // ButtonTextFont for buttons
 var ButtonTextFont font.Face
@@ -25,16 +29,21 @@ var ListItemTextFont font.Face
 var ListItemSubTextFont font.Face
 
 func deSetupFonts() {
-	font, err := freetype.ParseFont(goregular.TTF)
+	font, err := freetype.ParseFont(robotoregular.TTF)
 	if err != nil {
 		panic(err)
 	}
-	fontB, err := freetype.ParseFont(gobold.TTF)
+	fontB, err := freetype.ParseFont(robotobold.TTF)
+	if err != nil {
+		panic(err)
+	}
+	fontI, err := freetype.ParseFont(robotoitalic.TTF)
 	if err != nil {
 		panic(err)
 	}
 	PageTitleFont = integration.CreateTTFFont(font, integration.DPIPixels, float64(frenyard.Scale(DesignScale, 24)))
 	GlobalFont = integration.CreateTTFFont(font, integration.DPIPixels, float64(frenyard.Scale(DesignScale, 16)))
+	GlobalItalicFont = integration.CreateTTFFont(fontI, integration.DPIPixels, float64(frenyard.Scale(DesignScale, 16)))
 	ButtonTextFont = integration.CreateTTFFont(fontB, integration.DPIPixels, float64(frenyard.Scale(DesignScale, 14)))
 
 	// 16dp

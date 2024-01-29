54d5e66e670e8fd369c6068ed1c8b734fc68b11b
Alyxia Sother <alyxia@riseup.net>
2024-01-29 12:26:12 +0100

    [example] Marginally better UI layout

--- example/screens/buttons.go
+++ example/screens/buttons.go
@@ -1,8 +1,10 @@
 package screens
 
 import (
+	"github.com/lexisother/frenyard"
 	"github.com/lexisother/frenyard/design"
 	"github.com/lexisother/frenyard/framework"
+	"github.com/lexisother/frenyard/integration"
 )
 
 var ScreenButtons []framework.FlexboxSlot
@@ -10,28 +12,18 @@ var ScreenButtons []framework.FlexboxSlot
 func SetupButtons() {
 	ScreenButtons = []framework.FlexboxSlot{
 		{
+			Grow: 1,
+		},
+		{
+			Element: framework.NewUILabelPtr(integration.NewTextTypeChunk("Simple buttons with themed colours.", design.GlobalFont), 0xFFFFFFFF, 0, frenyard.Alignment2i{}),
+		},
+		{
+			Basis: design.SizeMarginAroundEverything,
+		},
+		{
 			Element: framework.NewUIFlexboxContainerPtr(framework.FlexboxContainer{
 				DirVertical: false,
 				Slots: []framework.FlexboxSlot{
-					{
-						Element: framework.NewUIOverlayContainerPtr(design.ScrollboxExterior, []framework.UILayoutElement{
-							framework.NewUIFlexboxContainerPtr(framework.FlexboxContainer{
-								DirVertical: true,
-								Slots: []framework.FlexboxSlot{
-									{
-										Grow: 1,
-										Element: design.ButtonBar([]framework.UILayoutElement{
-											design.ButtonAction(design.ThemeOkActionButton, "Ok", func() {}),
-											design.ButtonAction(design.ThemeRemoveActionButton, "Remove", func() {}),
-											design.ButtonAction(design.ThemeUpdateActionButton, "Update", func() {}),
-											design.ButtonAction(design.ThemeImpossibleActionButton, "Disabled", func() {}),
-											design.ButtonAction(design.ThemePageActionButton, "Details", func() {}),
-										}),
-									},
-								},
-							}),
-						}),
-					},
 					{
 						Grow: 1,
 					},
@@ -39,9 +31,40 @@ func SetupButtons() {
 						Element: framework.NewUIOverlayContainerPtr(design.ScrollboxExterior, []framework.UILayoutElement{
 							framework.NewUIFlexboxContainerPtr(framework.FlexboxContainer{
 								DirVertical: true,
-								Slots:       []framework.FlexboxSlot{},
+								Slots: []framework.FlexboxSlot{
+									{
+										Element: design.ButtonAction(design.ThemeOkActionButton, "Ok", func() {}),
+									},
+									{
+										Grow: 1,
+									},
+									{
+										Element: design.ButtonAction(design.ThemeRemoveActionButton, "Remove", func() {}),
+									},
+									{
+										Grow: 1,
+									},
+									{
+										Element: design.ButtonAction(design.ThemeUpdateActionButton, "Update", func() {}),
+									},
+									{
+										Grow: 1,
+									},
+									{
+										Element: design.ButtonAction(design.ThemeImpossibleActionButton, "Disabled", func() {}),
+									},
+									{
+										Grow: 1,
+									},
+									{
+										Element: design.ButtonAction(design.ThemePageActionButton, "Details", func() {}),
+									},
+								},
 							}),
 						}),
+					},
+					{
+						Grow: 1,
 					},
 				},
 			}),
